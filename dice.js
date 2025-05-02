2122150dd87daf4be8b4519b8329cbcde45742d8
tallbird <47836219+tallbird@users.noreply.github.com>
2025-05-02 14:57:08 +0100

    renaming

--- dice.js
+++ dice.js
@@ -1,3 +1,53 @@
+<<<<<<< HEAD
+// need a JSON of skills / supports
+class Dice {
+    constructor(skill, resourceLayout = ['A', 'A', 'D', 'D', 'U', 'U'], number = 1, locked = false) {
+        this.skill = skill;
+        this.resourceLayout = resourceLayout;
+        this.number = number;
+        this.prevRolls = [6, 5]
+        this.locked = locked;
+    }
+
+    rollDice() {
+        this.number = Dice.randomDice();
+        return this.number
+    }
+
+    getBounces(numOfBounces) {
+        return Dice.getBounceArray(this.prevRolls[this.prevRolls.length - 1], this.number, numOfBounces)
+    }
+
+    
+    // pick a number from set. Default 1-6 can pass in [array of numbers] e.g [1,2,4]
+    static randomDice(set = [1, 2, 3, 4, 5, 6]) {
+        const random = Math.ceil(Math.random() * set.length);
+        //    console.log(set, random, set[random-1]);
+        return set[random - 1];
+    }
+
+    // generates a set excluding a single number to exclude   eg setExclude(4)  >> [1,2,3,  5,6]
+    static setExclude(exclude) {
+        return [1, 2, 3, 4, 5, 6].filter(v => v != exclude);
+    }
+
+    // generates a set excluding numbers   eg setExcludeArray([1,2,3])  >> [       4,5,6]
+    static setExcludeArray(excludeArray) {
+        return [1, 2, 3, 4, 5, 6].filter(v => !(excludeArray.includes(v)));
+    }
+
+    static getBounceArray(prevNumber, finalNumber, numOfBounces) {
+        let bounces = [Dice.randomDice(Dice.setExcludeArray([prevNumber, finalNumber]))];
+        for (let i = 0; i < numOfBounces - 1; i++) {
+            bounces.push(Dice.randomDice(Dice.setExcludeArray([prevNumber, finalNumber, bounces[bounces.length - 1]])));
+        }
+        // console.log(bounces);
+        return bounces;
+    }
+}
+
+//acts pool
+=======
 // need a JSON of skills / supports
 class Dice {
     constructor(skill, number = 1, locked = false) {
@@ -45,4 +95,5 @@ class Dice {
 }
 
 //acts pool
+>>>>>>> 8ac50f5e485fc2fc1c2b90c2e447338981d53fe2
 //offensive / defensive / utility
--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const rollBtn = document.querySelector('.roll');
+<<<<<<< HEAD
+const diceBox1 = document.getElementById('diceBox1');
+const diceBox2 = document.getElementById('diceBox2');
+const diceBox3 = document.getElementById('diceBox3');
+const diceBox4 = document.getElementById('diceBox4');
+const diceBox5 = document.getElementById('diceBox5');
+const diceBox6 = document.getElementById('diceBox6');
+
+const playerDiceBox = [diceBox1, diceBox2, diceBox3, diceBox4, diceBox5, diceBox6]
+=======
 const dice1 = document.getElementById('dice1');
 const dice2 = document.getElementById('dice2');
 const dice3 = document.getElementById('dice3');
@@ -7,6 +17,7 @@ const dice5 = document.getElementById('dice5');
 const dice6 = document.getElementById('dice6');
 
 const player = [dice1, dice2, dice3, dice4, dice5, dice6]
+>>>>>>> 8ac50f5e485fc2fc1c2b90c2e447338981d53fe2
 let lastDice = 1
 let dicePool = [new Dice('bite'), new Dice('dice1'), new Dice('dice2'), new Dice('dice3'), new Dice('dice4'), new Dice('dice5')]
 let currentDice = dicePool
@@ -26,31 +37,50 @@ function rollDiceTransition(screenDie, number) {
 
 }
 
+<<<<<<< HEAD
+function animateRoll(diceBox, dice) {
+    console.log(diceBox, dice)
+    let bounceArray = dice.getBounces(3);
+=======
 function animateRoll(screenDie, n) {
     console.log(screenDie, n)
     let bounceArray = Dice.getBounceArray(lastDice, n, 3);
+>>>>>>> 8ac50f5e485fc2fc1c2b90c2e447338981d53fe2
     //let bounceArray = Dice.getBounceArray(lastDice, n, 3);
     // console.log(bounceArray, n );
-    rollDiceTransition(screenDie, bounceArray[0]);
-    setTimeout(() => rollDiceTransition(screenDie, bounceArray[1]), 500);
-    setTimeout(() => rollDiceTransition(screenDie, bounceArray[2]), 1000);
-    setTimeout(() => rollDiceTransition(screenDie, n), 1500);
-    lastDice = n;
+    rollDiceTransition(diceBox, bounceArray[0]);
+    setTimeout(() => rollDiceTransition(diceBox, bounceArray[1]), 500);
+    setTimeout(() => rollDiceTransition(diceBox, bounceArray[2]), 1000);
+    setTimeout(() => rollDiceTransition(diceBox, dice), 1500);
+    lastDice = dice;
 }
 
+<<<<<<< HEAD
+function animateRollsArray(DiceBox, currentDice) {
+
+    console.log(DiceBox)
+    if (DiceBox.length != currentDice.length) console.log("array length problem");
+
+    DiceBox.forEach((die, i) => {
+=======
 function animateRollsArray(screenDice, currentDice) {
 
     console.log(screenDice)
     if (screenDice.length != currentDice.length) console.log("array length problem");
 
     screenDice.forEach((die, i) => {
+>>>>>>> 8ac50f5e485fc2fc1c2b90c2e447338981d53fe2
         animateRoll(die, currentDice[i]);
     });
 }
 
 // roll dice button
 function rollRandom() {
+<<<<<<< HEAD
+    animateRollsArray(playerDiceBox, currentDice);
+=======
     animateRollsArray(player, currentDice);
+>>>>>>> 8ac50f5e485fc2fc1c2b90c2e447338981d53fe2
 }
 
 // toggle border animation on/off on dice click
